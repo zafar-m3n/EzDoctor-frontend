1725c951592335b195899784964ad5c604d3f7cd
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-12 01:27:40 +0530

    15th commit- Doctors and users lists

--- src/pages/admin/Doctors.js
+++ src/pages/admin/Doctors.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
+import axios from "axios";
+import { Table } from "antd";
 
 const Doctors = () => {
+  const [doctors, setDoctors] = useState([]);
+  // Get Users
+  const getDoctors = async () => {
+    try {
+      const res = await axios.get("/api/v1/admin/getAllDoctors", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (res.data.success) {
+        setDoctors(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getDoctors();
+  }, []);
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text, record) => (
+        <span>
+          {record.firstName} {record.lastName}
+        </span>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          {record.status === "pending" ? (
+            <button className="btn btn-success">Approve</button>
+          ) : (
+            <button className="btn btn-danger">Reject</button>
+          )}
+        </div>
+      ),
+    },
+  ];
+
   return (
     <Layout>
       <h1>All Doctors</h1>
+      <Table columns={columns} dataSource={doctors} />
     </Layout>
   );
 };
