3dce77f9d9bee2f616d11c27ae6cc878518ae741
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-12 00:38:16 +0530

    14th commit- Doctors and users list UI

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
 import ApplyDoctor from "./pages/ApplyDoctor";
 import Notifications from "./pages/Notifications";
+import Users from "./pages/admin/Users";
+import Doctors from "./pages/admin/Doctors";
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
@@ -31,6 +33,22 @@ function App() {
               element={
                 <ProtectedRoute>
                   <ApplyDoctor />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute>
+                  <Users />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/doctors"
+              element={
+                <ProtectedRoute>
+                  <Doctors />
                 </ProtectedRoute>
               }
             />
