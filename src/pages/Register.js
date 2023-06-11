daf6217a4b4278ff951e5f5d3702c07325d8dc3c
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-11 11:19:53 +0530

    Third commit- register handler

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "../styles/LoginRegisterStyles.css";
-import { Form, Input } from "antd";
+import { Form, Input, message } from "antd";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
   const navigate = useNavigate();
   // Form Handler
-  const onfinishHandler = (values) => {
-    console.log(values);
+  const onfinishHandler = async (values) => {
+    try {
+      const res = await axios.post("/api/v1/user/register", values);
+      if (res.data.success) {
+        message.success("Registered Successfully!");
+        navigate("/login");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("Something went wrong");
+    }
   };
   return (
     <>
