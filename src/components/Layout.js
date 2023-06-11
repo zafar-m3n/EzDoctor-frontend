b9bfd58b5d85583a512361db1c06c5a9f0c65702
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-11 14:35:40 +0530

    Ninth commit- header and user based sidebar menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "../styles/LayoutStyles.css";
-import { SidebarMenu } from "../Data/data";
+import { adminMenu, userMenu } from "../Data/data";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { message } from "antd";
 
 const Layout = ({ children }) => {
+  const { user } = useSelector((state) => state.user);
   const location = useLocation();
+  const navigate = useNavigate();
+  //   Handle Logout
+  const handleLogout = () => {
+    localStorage.clear();
+    message.success("Logged out Successfully");
+    navigate("/login");
+  };
+
+  const SidebarMenu = user?.isAdmin ? adminMenu : userMenu;
   return (
     <>
       <div className="main">
@@ -26,10 +38,19 @@ const Layout = ({ children }) => {
                   </>
                 );
               })}
+              <div className={`menu-item`} onClick={handleLogout}>
+                <i className="fa-solid fa-right-from-bracket"></i>
+                <Link to="/login">Logout</Link>
+              </div>
             </div>
           </div>
           <div className="content">
-            <div className="header">Header</div>
+            <div className="header">
+              <div className="header-content">
+                <i class="fa-solid fa-bell"></i>
+                <Link to="/profile">{user?.name}</Link>
+              </div>
+            </div>
             <div className="body">{children}</div>
           </div>
         </div>
