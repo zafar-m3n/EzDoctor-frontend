37caa67a1874a8f554400efb60c691206bfec6d8
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-12 16:07:06 +0530

    18th commit- Update profile handler

--- src/pages/doctor/Profile.js
+++ src/pages/doctor/Profile.js
@@ -1,13 +1,51 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
-import { useSelector } from "react-redux";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { Col, Form, Input, Row, TimePicker, message } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../../redux/features/alertSlice";
+import moment from "moment";
 
 const Profile = () => {
   const { user } = useSelector((state) => state.user);
   const [doctor, setDoctor] = useState(null);
   const params = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleFinish = async (values) => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/doctor/updateProfile",
+        {
+          ...values,
+          userId: user._id,
+          timings: [
+            moment(values.timings[0]).format("HH:mm"),
+            moment(values.timings[1]).format("HH:mm"),
+          ],
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        message.success(res.data.message);
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+      message.error("Something went wrong!");
+    }
+  };
 
   const getDoctorDetails = async () => {
     try {
@@ -36,6 +74,122 @@ const Profile = () => {
   return (
     <Layout>
       <h1>Manage Profile</h1>
+      {doctor && (
+        <Form
+          layout="vertical"
+          onFinish={handleFinish}
+          className="m-3"
+          initialValues={{
+            ...doctor,
+            timings: [
+              moment(doctor.timings[0], "HH:mm"),
+              moment(doctor.timings[1], "HH:mm"),
+            ],
+          }}
+        >
+          <h4>Personal Details</h4>
+          <Row gutter={20}>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="First Name"
+                name="firstName"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="Your First Name" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Last Name"
+                name="lastName"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="Your Last Name" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="NIC"
+                name="nic"
+                required
+                maxLength={12}
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="Your NIC Number" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Email"
+                name="email"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="Your Email Address" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Address"
+                name="address"
+                required
+                maxlength={12}
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="Your Clinic Address" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Phone"
+                name="phone"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="Your Phone Number" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <h4>Professional Details</h4>
+          <Row gutter={20}>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Specialisation"
+                name="specialisation"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="Your Specialisation" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Price per Consultation"
+                name="fees"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="Your Fee Per Consultation" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item label="Timings" name="timings" required>
+                <TimePicker.RangePicker
+                  format="HH:mm"
+                  style={{ width: "100%" }}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <div className="d-flex justify-content-center">
+            <button className="btn btn-outline-light form-btn" type="submit">
+              Update
+            </button>
+          </div>
+        </Form>
+      )}
     </Layout>
   );
 };
