5f0340c2d2fa48b11fa9d40aa74d0d0c50deec32
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-12 17:51:07 +0530

    19th commit- Patient user homepage (Display all doctors)

---
+++ src/components/DoctorList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const DoctorList = ({ doctor }) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div
+        className="card m-2"
+        style={{ cursor: "pointer" }}
+        onClick={() => navigate(`/book-appointment/${doctor._id}`)}
+      >
+        <div className="card-header">
+          Dr. {doctor.firstName} {doctor.lastName}
+        </div>
+        <div className="card-body">
+          <p>
+            <b>Specialisation:</b> {doctor.specialisation}
+          </p>
+          <p>
+            <b>Fees per consultation:</b> Rs. {doctor.fees}
+          </p>
+          <p>
+            <b>Available Time:</b> {doctor.timings[0]} - {doctor.timings[1]}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DoctorList;
